0fa385aeecd3b6a1cdda0853c336a1d93634c8df
GruntovGD <gruntovgd@gmail.com>
2023-02-13 12:04:07 +0300

    solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {      
+    }
 
     setRange(min, max) {
-
+      this.min=min
+      this.max=max
+      return (this.min,this.max)
     }
 
     guess() {
-
+      this.gessed= Math.round((this.max + this.min)/2)
+      return this.gessed
     }
 
     lower() {
-
+      this.max = this.gessed
     }
 
     greater() {
-
+      this.min = this.gessed
     }
 }
 
